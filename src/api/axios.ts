f6253f8601e345d0c4614d0609009c83ee3608ba
resoniratech-svg <resoniratech@gmail.com>
2026-08-20 10:27:15 +0530

    Auto-redirect to login when JWT token expires (401 response)

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -50,4 +50,17 @@ api.interceptors.request.use(
   }
 );
 
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      if (window.location.pathname !== "/login") {
+        window.location.href = "/login";
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default api;
